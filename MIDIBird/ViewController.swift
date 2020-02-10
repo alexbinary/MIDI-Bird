86e0089afd083d94fa8bee592cf126abf93578bc
Alexandre Bintz <alexandre@bintz.io>
2020-02-10 09:27:55 +0100

    Initial Commit

---
+++ MIDIBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MIDIBird
+//
+//  Created by Alexandre Bintz on 10/02/2020.
+//  Copyright © 2020 Alexandre Bintz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
