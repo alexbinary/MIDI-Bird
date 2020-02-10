a991d47d54e19fd288dcf04a537001eb67613dbf
Alexandre Bintz <alexandre@bintz.io>
2020-02-10 11:12:47 +0100

    make character move horizontally

--- MIDIBird_Shared/GameScene.swift
+++ MIDIBird_Shared/GameScene.swift
@@ -6,22 +6,25 @@ import MIKMIDI
 class GameScene: SKScene {
     
     
-    let inputSensibility: CGFloat = 0.2 // point per velocity unit
+    let inputSensibility: CGFloat = 0.2 // Newton.seconds per input velocity unit
+    let initialHorizontalImpulseMagnitude: CGFloat = 5 // Newton.seconds
     
     let MIDIDeviceName = "Alesis Recital Pro "  // trailing space intentional
     
     
+    var defaultCamera: SKCameraNode!
     var characterNode: SKShapeNode!
     
 
     override func didMove(to view: SKView) {
         
-        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        defaultCamera = SKCameraNode()
+        self.addChild(defaultCamera)
+        self.camera = defaultCamera
         
         self.isPaused = true
         
         initCharacter()
-        
         connectToMIDIDevice()
     }
     
@@ -29,9 +32,7 @@ class GameScene: SKScene {
     func initCharacter() {
         
         characterNode = SKShapeNode(circleOfRadius: 10)
-        
         characterNode.physicsBody = SKPhysicsBody(circleOfRadius: 10)
-        
         self.addChild(characterNode)
     }
     
@@ -52,10 +53,16 @@ class GameScene: SKScene {
         
         if self.isPaused {
             self.isPaused = false
+            characterNode.physicsBody!.applyImpulse(CGVector(dx: initialHorizontalImpulseMagnitude, dy: 0))
             return
         }
         
-        characterNode.physicsBody!.velocity = CGVector.zero
+        characterNode.physicsBody!.velocity = CGVector(dx: characterNode.physicsBody!.velocity.dx, dy: 0)
         characterNode.physicsBody!.applyImpulse(CGVector(dx: 0, dy: CGFloat(velocity) * inputSensibility))
+    }
+    
+    
+    override func didFinishUpdate() {
+        defaultCamera.position = CGPoint(x: characterNode.position.x, y: 0)
     }
 }
