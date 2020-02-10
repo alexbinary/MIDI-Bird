68391dbad962984a654ef8671c3204d8c8983b05
Alexandre Bintz <alexandre@bintz.io>
2020-02-10 09:29:37 +0100

    add mac target

---
+++ MIDIBird_Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MIDIBird_Mac
+//
+//  Created by Alexandre Bintz on 10/02/2020.
+//  Copyright © 2020 Alexandre Bintz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
