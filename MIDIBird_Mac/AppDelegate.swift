67204c6327bf542652b80c549b835db6513b3ff0
Alexandre Bintz <alexandre@bintz.io>
2020-02-10 10:12:25 +0100

    remove boilerplate code

--- MIDIBird_Mac/AppDelegate.swift
+++ MIDIBird_Mac/AppDelegate.swift
@@ -1,26 +1,8 @@
-//
-//  AppDelegate.swift
-//  MIDIBird_Mac
-//
-//  Created by Alexandre Bintz on 10/02/2020.
-//  Copyright © 2020 Alexandre Bintz. All rights reserved.
-//
 
 import Cocoa
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
--- MIDIBird_iOS/ViewController.swift
+++ MIDIBird_iOS/ViewController.swift
@@ -1,20 +1,7 @@
-//
-//  ViewController.swift
-//  MIDIBird_iOS
-//
-//  Created by Alexandre Bintz on 10/02/2020.
-//  Copyright © 2020 Alexandre Bintz. All rights reserved.
-//
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
